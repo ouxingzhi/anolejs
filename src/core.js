0d86f45499849e8d52fed5ef8d0f26818bb4272b
ouxingzhi <ouxingzhi@vip.qq.com>
2013-01-31 23:57:11 +0800

    Update src/core.js
    实现javascript继承机制

--- src/core.js
+++ src/core.js
@@ -40,4 +40,48 @@ void function(window,document){
 	Anole.UseNameSpace('core').extend({
 		Type:Type
 	});
+	Anole.extend({
+		/**
+		 * 定义类
+		 × @param {Function} 可选 要继承的类
+		 * @param {Object} 	 必填 实现类的成员
+		 * @return {Function} 定义的类
+		 × @author ou xing zhi
+		 */
+		Class:(function(){
+			var slice = [].slice;
+			function bind(fun,obj){
+				var a = slice.call(arguments,2);
+				return function(){
+					var args = a.concat(slice.call(arguments,0));
+					fun.apply(obj,args);
+				};
+			}
+			function Class(){
+				var args = slice.call(arguments,0),
+					propertys = args.pop(),
+					parent = args.shift(),
+					empty = function(){}
+					Class = function(){
+						this.initialize.apply(this,arguments);
+					}
+				typeof parent != 'function' && (parent = function(){});
+				empty.prototype = parent.prototype;
+				Class.prototype = new empty();
+				var supInitialize = Class.prototype.initialize ;
+				for(var i in propertys) {
+					Class.prototype[i] = propertys[i];
+				}
+				if(supInitialize && Class.prototype.initialize){
+					var newInitialize = Class.prototype.initialize;
+					Class.prototype.initialize = function(){
+						newInitialize.apply(this,[bind(function(){supInitialize.apply(this,arguments);},this)].concat(slice.call(arguments,0)));
+					}
+				}
+				Class.prototype.constructor = parent;
+				return Class;
+			}
+			return Class;
+		})()
+	});
 }(window,document);
